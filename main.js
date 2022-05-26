f96211f63f31d2aa9dc81e265b8bf01cb8e5822b
amadolucas <92124872+amadolucas@users.noreply.github.com>
2022-05-26 10:00:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,9 +13,12 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.size(380,380);
+  video.hide();
+}
+
+function start() {
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
   document.getElementById("status").innerHTML = "Status: Detectando Objetos";
-video.hide();
 }
 
 function modelLoaded() {
